73d7a277be4c75e2e959af4179295d228134cd15
bumbleblym <bumbleblym@gmail.com>
2015-03-18 17:33:21 +0800

    feat: create table statements

---
+++ tables.sql
@@ -0,0 +1,89 @@
+CREATE TABLE User (
+  uid INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  hash VARCHAR(255) NOT NULL,
+  banned INT NOT NULL,
+  admin INT NOT NULL,
+  PRIMARY KEY (uid)
+);
+
+CREATE TABLE Post (
+  pid INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  submitted DATE NOT NULL,
+  hidden INT NOT NULL,
+  author INT NOT NULL,
+  PRIMARY KEY (pid),
+  FOREIGN KEY (author) REFERENCES User(uid)
+);
+
+CREATE TABLE Comment (
+  cid INT NOT NULL AUTO_INCREMENT,
+  content VARCHAR(255) NOT NULL,
+  submitted DATE NOT NULL,
+  hidden INT NOT NULL,
+  author INT NOT NULL,
+  parent INT NOT NULL,
+  PRIMARY KEY (cid),
+  FOREIGN KEY (author) REFERENCES User(uid),
+  FOREIGN KEY (parent) REFERENCES Post(pid)
+);
+
+CREATE TABLE Tag (
+  tid INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  PRIMARY KEY (tid)
+);
+
+CREATE TABLE PostTags (
+  pid INT NOT NULL,
+  tid INT NOT NULL,
+  PRIMARY KEY (pid, tid),
+  FOREIGN KEY (pid) REFERENCES Post(pid),
+  FOREIGN KEY (tid) REFERENCES Tag(tid)
+);
+
+CREATE TABLE Feed (
+  uid INT NOT NULL,
+  tid INT NOT NULL,
+  PRIMARY KEY (uid, tid),
+  FOREIGN KEY (uid) REFERENCES User(uid),
+  FOREIGN KEY (tid) REFERENCES Tag(tid)
+);
+
+CREATE Table PostVote (
+  uid INT NOT NULL,
+  pid INT NOT NULL,
+  value INT NOT NULL,
+  PRIMARY KEY (uid, pid),
+  FOREIGN KEY (uid) REFERENCES User(uid),
+  FOREIGN KEY (pid) REFERENCES Post(pid)
+);
+
+CREATE TABLE PostReport (
+  uid INT NOT NULL,
+  pid INT NOT NULL,
+  submitted DATE NOT NULL,
+  PRIMARY KEY (uid, pid),
+  FOREIGN KEY (uid) REFERENCES User(uid),
+  FOREIGN KEY (pid) REFERENCES Post(pid)
+);
+
+CREATE Table CommentVote (
+  uid INT NOT NULL,
+  cid INT NOT NULL,
+  value INT NOT NULL,
+  PRIMARY KEY (uid, cid),
+  FOREIGN KEY (uid) REFERENCES User(uid),
+  FOREIGN KEY (cid) REFERENCES Comment(cid)
+);
+
+CREATE TABLE CommentReport (
+  uid INT NOT NULL,
+  cid INT NOT NULL,
+  submitted DATE NOT NULL,
+  PRIMARY KEY (uid, cid),
+  FOREIGN KEY (uid) REFERENCES User(uid),
+  FOREIGN KEY (cid) REFERENCES Comment(cid)
+);
