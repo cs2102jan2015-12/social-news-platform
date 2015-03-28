d0711023dea074331aa3769e8a49b2b7d5e27dcb
hyungjon <hyungjon.kim@gmail.com>
2015-03-28 14:41:55 +0000

    Added functions regarding tags to addPost

--- application/models/post.php
+++ application/models/post.php
@@ -23,18 +23,44 @@ class Post
      * @param string $submitted date
      * @param string $uID author
      */
-    public function writePost($pID, $title, $content, $submitted, $uID) {
-        $sql = "INSERT INTO Post (pID, title, content, submitted, author) VALUES (:pID, :title, :content, :submitted, :author)";
+    public function writePost($title, $content, $submitted, $uID, $tags) { // $tags is an array
+        
+        $sql = "INSERT INTO Post (title, content, submitted, author) VALUES (:title, :content, :submitted, :author)";
         $query = $this->db->prepare($sql);
-        $parameters = array(':pID'=> $pID, ':title' => $title, ':content' => $content, ':submitted' => $submitted, ':author' => $uID,);
+        $parameters = array(':title' => $title, ':content' => $content, ':submitted' => $submitted, ':author' => $uID);
         $query->execute($parameters);
         // useful for debugging: you can see the SQL behind above construction by using:
         // echo '[ PDO DEBUG ]: ' . debugPDO($sql, $parameters);  exit();
+        $postID = $this->db->lastInsertId();
+        
+        foreach ($tags as $tag) {
+            // if statement for checking whether the tag is a new one
+            $sql = "SELECT tID FROM Tag GROUP BY name HAVING name = :tag";
+            $query = $this->db->prepare($sql);
+            $parameters = array(':tag' => $tag);
+            $query->execute($parameters);
+            $tagID = $query->fetch();
+            
+            if ($tagID = null) { // tag is new, create new entry
+                $sql = "INSERT INTO TAG (name) VALUES (:tag)";
+                $query = $this->db->prepare($sql);
+                $parameters = array(':tag' => $tag);
+                $query->execute($parameters);
+                $tagID = $this->db->lastInsertId();
+            }
+            
+            //create posttage entity for each pair
+            $sql = "INSERT INTO PostTags (pID, tID) VALUES (:pID, :tID)";
+            $query = $this->db->prepare($sql);
+            $parameters = array(':pID' => $postID, 'tID' => $tagID);
+            $query->execute($parameters);
+            
+        }
 
         if ($query->execute($parameters) > 0) { // If the query is successful...
             $sql = "SELECT * FROM Post WHERE pID = :pID";
             $query = $this->db->prepare($sql);
-            $query->execute(array(':pID' => $pID)); // Execute query first, then...
+            $query->execute(array(':pID' => $postID)); // Execute query first, then...
             $post = $query->fetch(PDO::FETCH_ASSOC); // Fetch the array of attributes of the user.
             return $post; // Return the post.
         }
