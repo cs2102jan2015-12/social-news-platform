cec152c61bb2966bbe08cb2fbced2fa2bb3a925e
Benjamin Lim <bumbleblym@gmail.com>
2015-04-06 09:46:52 +0000

    Remove pagination arrows

--- application/views/feed/feeds.php
+++ application/views/feed/feeds.php
@@ -4,23 +4,13 @@
   <div class="feeds">
     <nav>
       <ul class="pagination">
-        <li>
-          <a href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-            <?php
-              foreach ($feeds as $feed) {
-            ?>
-            <li><a href="<?php echo URL_WITH_INDEX_FILE; ?>feed/<?php echo $feed->tag ?>"><?php echo $feed->tag ?></a></li>
-            <?php
-              }
-            ?>
-        <li>
-          <a href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
+        <?php
+          foreach ($feeds as $feed) {
+        ?>
+        <li><a href="<?php echo URL_WITH_INDEX_FILE; ?>feed/<?php echo $feed->tag ?>"><?php echo $feed->tag ?></a></li>
+        <?php
+          }
+        ?>
       </ul>
     </nav>
   </div>
