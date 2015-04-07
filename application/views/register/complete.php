ae91865bf41507c0f9fc246ab4c14f4e11de9e92
Sun Wang Jun <sunxperous@gmail.com>
2015-04-07 15:47:10 +0000

    Remove unneeded files

--- application/views/register/complete.php
+++
@@ -1,7 +0,0 @@
-<?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
-
-<div class="container">
-    <h2>Registration complete, yay!</h2>
-    <p>If your registration is successful, you should be able to see...</p>
-    <p></p><?php echo var_dump($_SESSION); ?></p>
-</div>
